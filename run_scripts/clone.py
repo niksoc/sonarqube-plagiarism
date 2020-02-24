80b411d0021168c9e9078d8d4ff5514f79944e88
nikhil <nikhil_2811@hotmail.com>
2020-02-24 11:09:30 +0530

    Made clone script concurrent

--- run_scripts/clone.py
+++ run_scripts/clone.py
@@ -1,6 +1,10 @@
+
 import os
 import subprocess
 import sys
+import concurrent.futures
+
+from more_itertools.recipes import grouper
 
 absent = []
 
@@ -17,9 +21,14 @@ def get_repo(ssh_url):
     return ssh_url.split('/')[-1]
 
 
-for https_url in [
-        'https://git.abc.com/<username>/<project_name>',
-]:
+https_urls = [
+   "https://git.<abc>.com/<abc>",
+]
+
+def batch_clone_or_update(https_urls, update=False):
+    return [clone_or_update(https_url, update) for https_url in https_urls]
+
+def clone_or_update(https_url, update=False):
     ssh_url = https_to_ssh(https_url)
     username = get_username(ssh_url)
     repo = get_repo(ssh_url)
@@ -28,22 +37,29 @@ for https_url in [
         try:
             assert username in subprocess.run(
                 ['git', 'remote', '-v'], stdout=subprocess.PIPE, cwd='./' + dir_name).stdout.decode()
-            if len(sys.argv) > 1 and sys.argv[1] == '-u':
+            if update:
                 subprocess.run(['git', 'pull'], cwd='./' + dir_name)
         except AssertionError as e:
-            print(e, username)
+            print(e, username, '*'*30)
             raise
-        continue
+        return
     print(username)
-    subprocess.run(['git', 'clone', ssh_url])
     try:
-        os.rename(repo, dir_name)
+        os.mkdir(dir_name)
+        subprocess.run(['git', 'clone', ssh_url, dir_name])
+        # os.rename(repo, dir_name)
         assert username in subprocess.run(
             ['git', 'remote', '-v'], stdout=subprocess.PIPE, cwd='./' + dir_name).stdout.decode()
     except Exception as e:
         print('username', e)
-        absent += [username]
+        return username
+
+
+update = len(sys.argv) > 1 and sys.argv[1] == '-u'
+
+executor = concurrent.futures.ThreadPoolExecutor(10)
+futures = executor.map(clone_or_update, https_urls)
 
 print('-' * 80)
-print(absent)
+print([future for future in futures if future])
 
